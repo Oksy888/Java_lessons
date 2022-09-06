0bb702e4fe92ad78fa2d15480de55dc9026deda7
Oksy888 <103371949+Oksy888@users.noreply.github.com>
2022-09-06 21:27:00 +0300

    recursion

---
+++ src/SumRecursion.java
@@ -0,0 +1,16 @@
+public class SumRecursion {
+
+    public static double sum(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        else {
+            return n * n + sum(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+
+}
