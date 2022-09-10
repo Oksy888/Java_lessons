5ceeb64bb8710bb31c5b4450f705d8a335c24213
Oksy888 <103371949+Oksy888@users.noreply.github.com>
2022-09-10 17:24:16 +0300

    add Powers8BiteShift

--- src/PowersBiteShift.java
+++ src/PowersBiteShift.java
@@ -1,22 +1,17 @@
 import java.util.Scanner;
-
-public class Powers {
-
+public class PowersBiteShift {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите n в диапазоне от 0 до 10");
         int n = scanner.nextInt();
-        int power = 1;
-        final int k = 8;
-        if (n < 0 || n > 10) {
-            System.err.println("Неправильное значение n");
+        if (n < 0 || n > 30){
+            System.out.println("Не правильное значение n!");
         }
         else {
             for (int i = 0; i <= n; i++){
-                System.out.printf("8 ^ %d = %d\n", i, power);
-                power *= k;
+                System.out.printf("8 ^ %d = %d\n", i, 1 << i*3);
             }
         }
     }
 }
-
