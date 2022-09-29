dc5628593f382a3e4bb59bd9e91996efaab8f5c7
Oksy888 <103371949+Oksy888@users.noreply.github.com>
2022-09-29 12:56:28 +0300

    add new arrays

--- src/Oksana/SortArrayFirstFourAndSecondFour.java
+++ src/Oksana/SortArrayFirstFourAndSecondFour.java
@@ -1,22 +1,27 @@
 package Oksana;
 import java.util.Arrays;
 public class SortArrayFirstFourAndSecondFour {
-        public static void main(String[] args) {
-            int[] a = {18, 10, 8, 2, 3, 7, 94, 6, 15, 28};
-            System.out.println(Sort(a));
-        }
-            public static String Sort(int[] a){
+    public static void main(String[] args) {
+        int[] array = {18, 10, 8, 2, 3, 7, 28, 6, 4, 19};
+        System.out.println(sort(array));
+    }
 
-                Arrays.sort(a,0,4);
-                Arrays.sort(a, a.length-4, a.length);
+    public static String sort(int[] a) {
 
-        /*    for (int i = 6, i < a.length-1; i++){
+       Arrays.sort(a, 0, 4);
+        boolean mustSort;
+        do {
+            mustSort = false;
+            for (int i = a.length - 4; i < a.length - 1; i++) {
                 if (a[i] < a[i + 1]) {
                     int temp = a[i];
-                    a[i] = a[i+1];
-                    a[i+1] = temp;
-                  }
-            }*/
-                return Arrays.toString(a);
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    mustSort = true;
+                }
+            }
+        }
+        while (mustSort);
+        return Arrays.toString(a);
     }
 }
