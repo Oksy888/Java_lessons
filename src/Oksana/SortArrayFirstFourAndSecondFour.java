ad9409b7b746021b6befd49cc9ffcc2c27d2365b
Oksy888 <103371949+Oksy888@users.noreply.github.com>
2022-09-29 10:35:51 +0300

    add new arrays

--- src/Oksana/SortArrayFirstFourAndSecondFour.java
+++ src/Oksana/SortArrayFirstFourAndSecondFour.java
@@ -7,16 +7,10 @@ public class SortArrayFirstFourAndSecondFour {
 
     public static void Sort(int[] a){
 
-        int[] b =  Arrays.copyOfRange(a, 0, 4);
-        Arrays.sort(b);
-        int[] bb = Arrays.copyOfRange(a, a.length-4, a.length);
-        Arrays.sort(bb);
-        String arr = Arrays.toString(b);
-        String arr2 = Arrays.toString(bb);
-        StringBuffer sb = new StringBuffer(arr2);
-        sb.reverse();
-        System.out.println(arr + arr2);
+        Arrays.sort(a,0,4);
+        Arrays.sort(a, a.length-4, a.length);
 
+        System.out.println(Arrays.toString(a));
     }
     public static void main(String[] args) {
         int[] a = {18, 10, 8, 2, 3, 7, 94, 6, 15,28};
